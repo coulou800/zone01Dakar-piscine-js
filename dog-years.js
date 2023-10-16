28c21e3853baaba68f42ca2e91e2697934ab7ee7
bcoulibal <coulou789@gmail.com>
2023-10-16 17:27:17 +0000

    dogyear

--- dog-years.js
+++ dog-years.js
@@ -1,6 +1,6 @@
 function dogYears(planet,age) {
     let earthSec = 31557600
-    map = {
+    let map = {
         earth: 1.0,
         mercury: 0.2408467,
         venus: 0.61519726,
@@ -10,8 +10,8 @@ function dogYears(planet,age) {
         uranus: 84.016846,
         neptune: 164.79132
     }
-    dogYearEarth = (age/earthSec)*7
-    dogYeaPlanet = map[planet]*dogYearEarth
+    let dogYearEarth = (age/earthSec)*7
+    let dogYeaPlanet = map[planet]*dogYearEarth
 
     return dogYeaPlanet.toFixed(2)
 
