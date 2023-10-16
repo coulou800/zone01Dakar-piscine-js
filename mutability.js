9499c3b5d051159a0f032df43ac4f4a883ef0cfb
bcoulibal <coulou789@gmail.com>
2023-10-16 15:18:39 +0000

    mutability

--- mutability.js
+++ mutability.js
@@ -1,5 +1,6 @@
 let clone1 = new Object()
 let clone2 = new Object()
+let samePerson = new Object()
 clone1 = Object.assign(clone1, person)
 clone2 = Object.assign(clone2, person)
 samePerson = person
