45550ae661f0a3c8e185b901653b2fd4259b3f85
bcoulibal <coulou789@gmail.com>
2023-10-16 19:58:23 +0000

    minmax

---
+++ min-max.js
@@ -0,0 +1,13 @@
+function min(params,params1) {
+    if (params >params1) {
+        return params1
+    }
+    return params
+}
+
+function max(params,params1) {
+    if (params >params1) {
+        return params
+    }
+    return params1
+}
