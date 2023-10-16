0807dd1a9d83dc432b80c7dbb6afbb2df57711ed
bcoulibal <coulou789@gmail.com>
2023-10-16 15:32:19 +0000

    lastfirstkiss

---
+++ last-first-kiss.js
@@ -0,0 +1,10 @@
+function first(params) {
+    return params[0]
+}
+function last(params) {
+    return params[params.length-1]
+}
+
+function last(params) {
+    return [last(params),first(params)]
+}
