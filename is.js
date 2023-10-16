319f7dc8b32689f92ca706b157b55226879a22f9
bcoulibal <coulou789@gmail.com>
2023-10-16 20:48:47 +0000

    is

---
+++ is.js
@@ -0,0 +1,44 @@
+const is = {}
+
+is.num = (n)=> {
+    return typeof n == "number"
+}
+
+is.nan = (n) => {
+    return Number.isNaN(n)
+}
+
+is.str = (n)=>{
+    return typeof n == "string"
+}
+
+is.bool = (n)=>{
+    return typeof n == "boolean"
+}
+
+is.undef = (n)=>{
+    return typeof n == "undefined"
+}
+
+is.def = (n)=>{
+    return typeof n != "undefined"
+}
+is.arr = (n)=>{
+    return Array.isArray(n)
+}
+
+is.obj = (n)=>{
+    return typeof n == "Object"
+}
+
+is.fun = (n)=>{
+    return typeof n == "function"
+}
+
+is.truthy = (n)=>{
+    return n && "dog" == "dog" ? true : false
+}
+
+is.falsy = (n)=> {
+    return !is.truthy(n)
+}
