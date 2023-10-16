4b57c4752e7b481d6a8cf0992d554ab01a11e5dd
bcoulibal <coulou789@gmail.com>
2023-10-16 15:09:23 +0000

    mutability

--- mutability.js
+++ mutability.js
@@ -2,6 +2,12 @@ const person = {
     name: 'Rick',
     age: 77,
     country: 'US',
-  }
+}
+let clone1 = new Object()
+let clone2 = new Object()
+clone1 = Object.assign(clone1, person)
+clone2 = Object.assign(clone2, person)
+samePerson = person
 
-  
+person.age++
+person.country = 'FR'
